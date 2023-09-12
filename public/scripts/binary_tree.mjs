7fe41e4b7b5743f64398e51453d2eb32222ba709
Boyszy <boldizsarbanfia@Boyszys-Air.lan>
2023-09-12 17:40:19 +0100

    tree_traversal

--- public/scripts/binary_tree.mjs
+++ public/scripts/binary_tree.mjs
@@ -39,14 +39,108 @@ class BinarySearchTree {
             }
         }
     }
+    get(value){
+        if(this.root === null) return false; // if there is no root return false
+        if(this.root.value === value) return this.root; // if the root is the value return the root
+        let current = this.root; // set the current node to the root
+        let found = false; // set found to false
+        while(current && !found){ // while there is a current node and found is false
+            if(value < current.value){ // if the value is less than the current node
+                current = current.left; // set the current node to the left node
+            }else if(value > current.value){ // if the value is greater than the current node
+                current = current.right; // set the current node to the right node
+            }else{
+                found = true; // set found to true
+            }
+        }
+        if(!found) return false; // if found is false return false
+        return current; // return the current node
+    }
+    contains(value){
+        if(this.root === null) return false; // if there is no root return false
+        if(this.root.value === value) return this.root; // if the root is the value return the root
+        let current = this.root; // set the current node to the root
+        let found = false; // set found to false
+        while(current && !found){ // while there is a current node and found is false
+            if(value < current.value){ // if the value is less than the current node
+                current = current.left; // set the current node to the left node
+            }else if(value > current.value){ // if the value is greater than the current node
+                current = current.right; // set the current node to the right node
+            }else{
+                return true; // return true
+            }
+        }
+        return false; // return false
+    }
+    /////////////////////// Tree Traversal /////////////////////// still within the BinarySearchTree class
+    BreadthFirstSearch(){
+
+        let node = this.root;
+        let data = [];
+        let queue = [];
+
+        queue.push(this.root); // add the root to the queue
+        while(queue.length){ // while there is something in the queue
+            let node = queue.shift(); // remove the first item in the queue
+            data.push(node.value); // adding in to the list that we are returning
+            if(node.left) queue.push(node.left); // if there is a left node add it to the queue
+            if(node.right) queue.push(node.right); // if there is a right node add it to the queue
+        }
+        return data;
+    }
+
+    DepthFirstSearchPreOrder(){
+        let data = [];
+
+        function traverse(node){
+            data.push(node.value); // add the node to the list
+            if(node.left) traverse(node.left); // if there is a left node traverse the left node
+            if(node.right) traverse(node.right); // if there is a right node traverse the right node
+        }
+        traverse(this.root); // start at the root
+        return data;
+    }
+
+    DepthFirstSearchPostOrder(){
+        let data = [];
+
+        function traverse(node){
+            if(node.left) traverse(node.left); // if there is a left node traverse the left node
+            if(node.right) traverse(node.right); // if there is a right node traverse the right node
+            data.push(node.value); // add the node to the list
+        }
+        traverse(this.root); // start at the root
+        return data;
+    }
+
+    DepthFirstSearchInOrder(){
+        let data = [];
+
+        function traverse(node){
+            if(node.left) traverse(node.left); // if there is a left node traverse the left node
+            data.push(node.value); // add the node to the list
+            if(node.right) traverse(node.right); // if there is a right node traverse the right node
+        }
+        traverse(this.root); // start at the root
+        return data;
+    }
 }
 
 let tree = new BinarySearchTree();
 
 tree.insert(10);
-tree.insert(5);
-tree.insert(13);
-tree.insert(11);
-tree.insert(2);
-tree.insert(16);
-console.log(tree);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+console.log(tree.BreadthFirstSearch());
+console.log(tree.DepthFirstSearchPreOrder());
+console.log(tree.DepthFirstSearchPostOrder());
+console.log(tree.DepthFirstSearchInOrder());
+
+
+
+
+///////////// Tree Traversal ///////////// Tree Traversal//////////////////////// Tree Traversal//// 
+
