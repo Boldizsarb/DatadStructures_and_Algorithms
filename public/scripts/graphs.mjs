54afc5f52b17e3cd7367954d0872e505b7beb278
Boyszy <boldizsarbanfia@Boyszys-MacBook-Air.local>
2023-09-17 07:14:04 +0100

    graph_traversal

--- public/scripts/graphs.mjs
+++ public/scripts/graphs.mjs
@@ -28,6 +28,42 @@ class Graph {
         }
         delete this.adjacencyList[vertex] // delete the vertex
     }// remove a vertex from the adjacency list
+    depthFirstRecursive(start){
+        const result = [] // create a result array
+        const visited = {} // create a visited vertex object 
+        const adjacencyList = this.adjacencyList // create a variable to store the adjacency list
+
+        function dfs(vertex){ // create a dfs function
+            if(!vertex) return null // if there is no vertex, return null // base case
+            visited[vertex] = true // mark the vertex as visited
+            result.push(vertex) // push the vertex into the result array
+            adjacencyList[vertex].forEach(neighbor => { // for each neighbor of the vertex
+                if(!visited[neighbor]){ // if the neighbor has not been visited
+                    return dfs(neighbor) // recursively call dfs on the neighbor
+                }
+            })
+        }dfs(start) // invoke the dfs function on the start vertex
+        return result // return the result array
+    } // traverse the graph recursively
+    depthFirstIterative(start){
+        const stack = [start] // create a stack with the start vertex
+        const result = [] // create a result array
+        const visited = {} // create a visited vertex object
+        let currentVertex // create a current vertex variable
+        visited[start] = true // mark the start vertex as visited
+        while(stack.length){ // while the stack is not empty
+            currentVertex = stack.pop() // pop off the last element in the stack
+            result.push(currentVertex) // push the current vertex into the result array
+            this.adjacencyList[currentVertex].forEach(neighbor => { // for each neighbor of the current vertex
+                if(!visited[neighbor]){ // if the neighbor has not been visited
+                    visited[neighbor] = true // mark the neighbor as visited
+                    stack.push(neighbor) // push the neighbor into the stack
+                }
+            })
+        }
+        return result // return the result array
+    } // traverse the graph iteratively
+    
 
 }
 let g = new Graph()
@@ -37,7 +73,4 @@ g.addVertex("Aspen")
 g.addEdge("Tokyo", "Dallas")
 g.addEdge("Dallas", "Aspen")
 console.log(g)
-g.removeEdge("Tokyo", "Dallas")
-console.log(g)
-g.removeVertex("Dallas")
-console.log(g)
+console.log(g.depthFirstRecursive("Tokyo"))
